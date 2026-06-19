a3f7566fa389b9c8688398aa0634094b409fed20
hubievs13-web <hubievs13@gmail.com>
2026-06-19 23:37:33 +0300

    Sort main imports for start welcome change

--- src/crypto_flow_bot_v2/main.py
+++ src/crypto_flow_bot_v2/main.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import os
 
-from crypto_flow_bot_v2.config import DEFAULT_CONFIG_PATH, BotConfig, load_config
+from crypto_flow_bot_v2.config import BotConfig, DEFAULT_CONFIG_PATH, load_config
 from crypto_flow_bot_v2.live_runner import LiveAlertRunner
 from crypto_flow_bot_v2.logging import configure_logging, get_logger
 from crypto_flow_bot_v2.telegram import TelegramAlertService, TelegramAlertStatus
