f31686b085f01bc91dd39e3d8897bcb77d8401a0
hubievs13-web <hubievs13@gmail.com>
2026-06-30 05:15:52 +0300

    Install cycle summary diagnostics

--- src/crypto_flow_bot_v2/__init__.py
+++ src/crypto_flow_bot_v2/__init__.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from crypto_flow_bot_v2.candidate_engine_result_trace import install_candidate_engine_result_trace
+from crypto_flow_bot_v2.live_cycle_decision_summary import install_live_cycle_decision_summary
 from crypto_flow_bot_v2.live_symbol_decision_trace import install_live_symbol_decision_trace
 from crypto_flow_bot_v2.telegram import (
     TelegramAlertResult,
@@ -27,3 +28,4 @@ def _suppress_no_trade_diagnostic(
 setattr(TelegramAlertService, "send_no_trade_diagnostic", _suppress_no_trade_diagnostic)
 install_live_symbol_decision_trace()
 install_candidate_engine_result_trace()
+install_live_cycle_decision_summary()
